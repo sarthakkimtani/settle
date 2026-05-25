646ac1f0204e5bb404f3e7b5fecbc43f1c70f98b
Sarthak Kimtani <sarthakkimtani@outlook.com>
2026-05-25 19:46:59 +0530

    feat: setup clerk & convex

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
+import { ClerkProvider, useAuth } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+import AppLayout from "@/components/layout/app-layout";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL as string);
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <AppLayout />
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
